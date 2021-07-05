3ed873da694eb2e5a1253468c7babfa4e77d4442
Alison Haire <alisonhaire@alisons-mbp.lan>
2021-07-05 15:31:53 +1000

    Fix title and favicon issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "./styles/App.css";
+import { useEffect } from "react";
+import Head from "next/head";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.title = "Chainlink Lottery";
+  });
+  return (
+    <Component {...pageProps}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </Component>
+  );
 }
+
+export default MyApp;
