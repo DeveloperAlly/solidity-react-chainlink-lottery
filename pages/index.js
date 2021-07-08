85936bfcfddb68b8ae27644da561b0454cb66ac9
Alison Haire <alisonhaire@alisons-mbp.lan>
2021-07-08 16:23:59 +1000

    call data after picking winner

--- pages/index.js
+++ pages/index.js
@@ -254,7 +254,7 @@ function App(props) {
                 </>
               ),
             });
-            // fetchData();
+            fetchData();
           })
           .catch((err) =>
             setTransactionState({
